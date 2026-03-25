9d027cdd5f47dee6805af8f20ce162b5754e371e
1nb-nt <nitishbalaji1@gmail.com>
2026-03-25 14:02:22 +0530

    Rep

---
+++ WEEK1.2/Exe-7.js
@@ -0,0 +1,10 @@
+const objArray=[
+  {name:"Nithish",spec:"INT"},{
+    name:"Varun",spec:"CSE"},{
+      name:"Suresh",spec:"ECE"},{
+        name:"Ramesh",spec:"MECH"
+  }
+]
+for(let i=0;i<objArray.length;i++){
+console.log(objArray[i]["name"]);
+}
