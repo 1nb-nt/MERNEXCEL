b25ca1000b511b56d47091c48e4d61dea1de756d
1nb-nt <nitishbalaji1@gmail.com>
2026-03-30 16:44:58 +0530

    Array methods

---
+++ WEEK1.3/Exe-6.js
@@ -0,0 +1,16 @@
+console.log(parseInt("45efcrffe"));
+console.log(parseFloat("45.25633sxskbd"));
+ 
+const arr=[1,2,3,4,5,];
+arr.push(6);
+console.log(arr);
+
+const vrr=[1,2,3,4,5];vrr.pop(5);console.log(vrr);
+
+const dna=[2,4,6,8,9];
+dna.shift();
+console.log(dna);
+
+const mna=[2,4,6,8,9];
+mna.unshift(1);
+console.log(mna);
