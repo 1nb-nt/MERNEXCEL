ef1b87224c5827943c9e5a8201421ea56a7cca1c
1nb-nt <nitishbalaji1@gmail.com>
2026-03-30 11:25:37 +0530

    DOne

---
+++ WEEK1.3/Exe-2.js
@@ -0,0 +1,6 @@
+function FinalIndex(str,target){
+console.log("Obtained String",str)
+console.log("INdex:",str.lastIndexOf(target))
+}
+
+FinalIndex("Messi Messi Messi Ankara Messi Ankara Messi Ankara MesSi","sSi")
