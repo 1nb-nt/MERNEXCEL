95851c1d541122a583aade3888628cbce5fa34d2
1nb-nt <nitishbalaji1@gmail.com>
2026-04-01 11:03:44 +0530

    CallB

---
+++ WEEK1.3/Exe-7.js
@@ -0,0 +1,14 @@
+function h1(){
+    console.log("hello sivaji");
+}
+
+function h2(){
+    console.log("Cool Buddy ");
+}
+
+function callFunc(fn){
+  fn();
+}
+return callFunc(h2);
+
+//Example of call back function
