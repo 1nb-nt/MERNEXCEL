96c82ea900afa0d4f3ca31b44b266d3042e8f1ef
1nb-nt <nitishbalaji1@gmail.com>
2026-04-01 10:51:09 +0530

    Nithish

--- WEEK1.3/Exe-6.js
+++ WEEK1.3/Exe-6.js
@@ -14,3 +14,8 @@ console.log(dna);
 const mna=[2,4,6,8,9];
 mna.unshift(1);
 console.log(mna);
+
+const arr1=[1,2,3,4,5];
+const arr2=[6,7,8,9,10];
+  const arr3 = arr1.concat(arr2);
+  console.log(arr3)
