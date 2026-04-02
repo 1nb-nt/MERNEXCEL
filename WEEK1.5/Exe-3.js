e9796801092a9dcf858f358c1c0ffa7dbd6beda8
1nb-nt <nitishbalaji1@gmail.com>
2026-04-02 15:24:20 +0530

    Async

---
+++ WEEK1.5/Exe-3.js
@@ -0,0 +1,11 @@
+console.log("I AM BATMAN");
+
+setTimeout(function(){
+  console.log("I am not the pain , I am the Vengence");
+},10000);
+
+let a=0;
+for(let i=0; i<100; i++){
+  a+=i;
+}
+console.log(a);
