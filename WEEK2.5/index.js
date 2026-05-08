e2725131d6eb2ca32087b591934ca060aab9626b
1nb-nt <nitishbalaji1@gmail.com>
2026-05-08 11:48:10 +0530

    Express with query parameters

---
+++ WEEK2.5/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+function sum (a){
+  let ans = 0;
+  for(let i =0;i<=a;i++){
+    ans+=i;
+  } return ans; 
+}
+app.get("/",function(req,res){
+  const a = req.query.a;
+  const ans = sum(a);
+  res.send ("The sum of 1 to " + a + " is " + ans);
+})
+app.listen(2000);
